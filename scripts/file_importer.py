3ad719976a347de07692a5cab4b3bc001186fdb4
Jim Maddock <skimaddster@gmail.com>
2016-02-03 09:45:27 -0600

    updated file_importer to run on hyak

--- scripts/file_importer.py
+++ scripts/file_importer.py
@@ -15,7 +15,6 @@ class File_Importer(object):
     def __init__(self,wiki_name,db_path=None):
         basic.log('creating importer...')
         self.wiki_name = wiki_name
-        self.dh = None
         if not db_path:
             self.db_path = os.path.join(os.path.dirname(__file__),os.pardir,'db/') + wiki_name
         else:
@@ -26,91 +25,6 @@ class File_Importer(object):
         if not os.path.exists(self.db_path):
             print('creating dir: %s' % self.db_path)
             os.makedirs(self.db_path)
-
-    def single_import_from_dump(self,f_in,f_out,v=False):
-        inserted_count = 0
-        self.dh = single_dump_handler.Single_Dump_Handler(self.wiki_name,f_in)
-        db_file = open(f_in,'w')
-        db_file.write('"page_id","namespace","title","user_text","user_id","revert","ts"\n')
-        self.dh.process_dump()
-        for i,page in enumerate(self.dh.dump):
-            if page.namespace == 1 or page.namespace == 0:
-                self.create_csv_document(page,db_file)
-                inserted_count += 1
-                if v and inserted_count % 1000 == 0 and inserted_count != 0:
-                    basic.log('inserted (insert) %s documents' % inserted_count)
-            if i % 1000 == 0 and i != 0 and v:
-                basic.log('processed (insert) %s documents' % i)
-        basic.write_log('inserted %s documents' % inserted_count)
-
-            
-    def import_from_dump(self,v=False):
-        inserted_count = 0
-        self.dh = basic.Dump_Handler(self.wiki_name,history=True)
-        db_file_name = '%s/raw_edits.csv' % self.db_path
-        db_file = open(db_file_name,'w')
-        db_file.write('"page_id","namespace","title","user_text","user_id","revert","ts"\n')
-        while True:
-            try:
-                self.dh.next_dump()
-            except OSError:
-                basic.log('file does not exist (import finished?), exiting importer...')
-                basic.write_log('file does not exist (import finished?), exiting importer...')
-                return True
-            
-            for i,page in enumerate(self.dh.dump):
-                if page.namespace == 1 or page.namespace == 0:
-                    self.create_csv_document(page,db_file)
-                    inserted_count += 1
-                    if v and inserted_count % 1000 == 0 and inserted_count != 0:
-                        basic.log('inserted (insert) %s documents' % inserted_count)
-                if i % 1000 == 0 and i != 0 and v:
-                    basic.log('processed (insert) %s documents' % i)
-            basic.write_log('inserted %s documents' % inserted_count)
-
-    def import_csv_from_file(self):
-        self.db = pd.read_csv(self.db_file_name)
-
-    def create_json_document(self,page):
-        rt = basic.Revert_Tracker()
-        d = {}
-        d['page_id'] = page.id
-        d['namespace'] = page.namespace
-        d['title'] = page.title.split(':')[-1]
-        d['linked_pages'] = []
-        d['rev'] = []
-        for rev in page:
-            r = {}
-            r['user_text'] = rev.contributor.user_text
-            r['user_id'] = rev.contributor.id
-            r['revert'] = rt.is_revert(rev.sha1)
-            r['ts'] = str(datetime.datetime.fromtimestamp(rev.timestamp))
-            d['rev'].append(r)
-        return d
-
-    def create_csv_document(self,page,db_file):
-        rt = basic.Revert_Tracker()
-        d = {}
-        d['page_id'] = page.id
-        d['namespace'] = page.namespace
-        d['title'] = page.title.split(':')[-1].replace('"','')
-        for rev in page:
-            r = {}
-            if rev.contributor.user_text:
-                r['user_text'] = rev.contributor.user_text.replace('"','')
-            else:
-                r['user_text'] = rev.contributor.user_text
-            r['user_id'] = rev.contributor.id
-            r['revert'] = rt.is_revert(rev.sha1)
-            r['ts'] = str(datetime.datetime.fromtimestamp(rev.timestamp))
-            result = '%s,%s,"%s","%s",%s,"%s","%s"\n' % (d['page_id'],
-                                                         d['namespace'],
-                                                         d['title'],
-                                                         r['user_text'],
-                                                         r['user_id'],
-                                                         r['revert'],
-                                                         r['ts'])
-            db_file.write(result)
 
     def link_documents(self,v=False):
         f_in_name = '%s/edit_counts.csv' % self.db_path
@@ -126,17 +40,18 @@ class File_Importer(object):
         result = result0.append(result1)
         if v:
             print(result)
+        result['page_id'] = result.index
         result_path = '%s/linked_edit_counts.csv' % (self.db_path)
         columns = ['title','namespace','len','no_revert_len','linked_id']
         result.to_csv(result_path,na_rep='NONE',columns=columns,encoding='utf-8')
         return result
         #print(result.loc[(result['title'] == 'April')])
-        #print(result.loc[result['page_id.1'] == 1])
+        #print(result.loc[result['page_id.1'] == 1])        
         
-        
-    def add_rev_size(self,v=False):
-        f_in_name = '%s/raw_edits.csv' % self.db_path
+    def rev_size(self,v=False):
+        f_in_name = '%s/combined_raw_edits.csv' % self.db_path
         f_in = pd.read_csv(f_in_name)
+        print(f_in.loc[(f_in['page_id'] == 337753)])
         count = 0
         nr = f_in.loc[(f_in['revert'] == False)]
         df = f_in[['page_id','title','namespace']].drop_duplicates(subset='page_id').set_index('page_id',drop=False)
@@ -148,14 +63,10 @@ class File_Importer(object):
         result_path = '%s/edit_counts.csv' % (self.db_path)
         columns = ['title','namespace','len','no_revert_len']
         result.to_csv(result_path,na_rep='NONE',columns=columns,encoding='utf-8')
-        print(result.loc[(result['title'] == 'April') & (result['namespace'] == 0)])
         if v:
             print(result)
         return result
-
-    def test(self):
-        print(f_in.loc[(f_fin['title'] == 'April')])
-        
+    
 '''
 class User_Db_Importer(Db_Importer):
     def __init__(self,wiki_name):
@@ -234,20 +145,18 @@ class User_Db_Importer(Db_Importer):
             
 def main():
     parser = argparse.ArgumentParser(description='process wiki data')
-    parser.add_argument('lang')
+    parser.add_argument('-l','--lang')
+    parser.add_argument('-b','--base_dir')
+    #parser.add_argument('-i','--infile')
+    #parser.add_argument('-o','--outfile')
     args = parser.parse_args()
 
-    #fi = File_Importer(args.lang)
-    #fi.import_from_dump()
-    #fi.import_csv_from_file()
-    #fi.test()
-    #fi.add_rev_size()
-    #fi.link_documents()
-    #dbi.link_documents()
-    #dbi.add_rev_size(v=True)
-    #udi = User_Db_Importer(lang)
-    #udi.make_user_documents(v=True)
-    #udi.create_edit_counts()
+    if args.base_dir:
+        fi = File_Importer(args.lang,args.base_dir)
+    else:
+        fi = File_Importer(args.lang)
+    fi.rev_size()
+    fi.link_documents()
                 
 if __name__ == "__main__":
     main()
