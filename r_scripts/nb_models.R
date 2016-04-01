a646b11005e4e75a785e88da33f0b68a2f5d4f8a
Jim Maddock <skimaddster@gmail.com>
2016-04-01 09:18:29 -0500

    added conditionals and looping to language model script

--- r_scripts/nb_models.R
+++ r_scripts/nb_models.R
@@ -1,14 +1,12 @@
 require(foreign)
-require(ggplot2)
 require(MASS)
-require(quantreg)
 
 ## read csv from file
 args <- commandArgs(trailingOnly = TRUE)
 print(args[1])
 dat <- read.csv(args[1])
 
-## factor language varriable into dummies
+## factor language varriable into dummie
 dat <- within(dat, lang <- factor(lang))
 ## choose which dummy to use as reference
 dat <- within(dat, lang <- relevel(lang, ref = 'de'))
@@ -20,15 +18,8 @@ print("base model")
 summary(nb1 <- glm.nb(len_1 ~ num_editors_1 + tds_1, data = dat, maxit = 100))
 ## added article controls
 print("## article level controls (number of editors)")
-summary(nb2 <- glm.nb(len_1 ~ tds_1 + num_editors_1 + num_editors_0 + tds_1, data = dat, maxit = 100))
+summary(nb2 <- glm.nb(len_1 ~ tds_1 + num_editors_1 + num_editors_0 + tds_0, data = dat, maxit = 100))
 print("## article level controls (number of edits)")
-summary(nb3 <- glm.nb(len_1 ~ tds_1 + num_editors_1 + len_0 + tds_1, data = dat, maxit = 100))
+summary(nb3 <- glm.nb(len_1 ~ tds_1 + num_editors_1 + len_0 + tds_0, data = dat, maxit = 100))
 print("## article level controls (all)")
-summary(nb4 <- glm.nb(len_1 ~ tds_1 + num_editors_1 + len_0 + num_editors_0 + tds_1, data = dat, maxit = 100))
-## added langs
-print("## full model (number of editors)")
-summary(nb5 <- glm.nb(len_1 ~ tds_1 + num_editors_1 + num_editors_0 + tds_1 + lang, data = dat, maxit = 100))
-print("## full model (number of edits)")
-summary(nb6 <- glm.nb(len_1 ~ tds_1 + num_editors_1 + len_0 + tds_1 + lang, data = dat, maxit = 100))
-print("## full model (all)")
-summary(nb7 <- glm.nb(len_1 ~ tds_1 + num_editors_1 + len_0 + num_editors_0 + tds_1 + lang, data = dat, maxit = 100))
+summary(nb4 <- glm.nb(len_1 ~ tds_1 + num_editors_1 + len_0 + num_editors_0 + tds_0, data = dat, maxit = 100))
