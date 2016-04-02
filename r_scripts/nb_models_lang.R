1cbeeb93a0d83997d5e41994d270be2a5976ee0b
Jim Maddock <skimaddster@gmail.com>
2016-04-02 18:34:01 -0400

    deleted iteration

--- r_scripts/nb_models_lang.R
+++ r_scripts/nb_models_lang.R
@@ -7,25 +7,23 @@ args <- commandArgs(trailingOnly = TRUE)
 print(args[1])
 dat <- read.csv(args[1])
 
-## factor language varriable into dummie
+## factor language varriable into dummy
 dat <- within(dat, lang <- factor(lang))
 print("*** negative binomial models ***")
-for(i in unique(dat$lang)) {
-    ## choose which dummy to use as reference
-    print(i)
-    dat <- within(dat, lang <- relevel(lang, ref = i))    
-    ## added langs
-    if ("editors" %in% args) {
-        print("## full model (number of editors)")
-        print(summary(nb5 <- glm.nb(len_1 ~ tds_1 + num_editors_1 + num_editors_0 + tds_0 + lang, data = dat, maxit = 100)))
-    }
-    if ("edits" %in% args) {
-        print("## full model (number of edits)")
-        print(summary(nb6 <- glm.nb(len_1 ~ tds_1 + num_editors_1 + len_0 + tds_0 + lang, data = dat, maxit = 100)))
-    }
 
-    if ("all" %in% args) {
-        print("## full model (all)")
-        print(summary(nb7 <- glm.nb(len_1 ~ tds_1 + num_editors_1 + len_0 + num_editors_0 + tds_0 + lang, data = dat, maxit = 100)))
-    }
+## choose which dummy to use as reference
+dat <- within(dat, lang <- relevel(lang, ref = 'de'))    
+## added langs
+if ("editors" %in% args) {
+    print("## full model (number of editors)")
+    print(summary(nb5 <- glm.nb(len_1 ~ tds_1 + num_editors_1 + num_editors_0 + tds_0 + lang, data = dat, maxit = 100)))
+}
+if ("edits" %in% args) {
+    print("## full model (number of edits)")
+    print(summary(nb6 <- glm.nb(len_1 ~ tds_1 + num_editors_1 + len_0 + tds_0 + lang, data = dat, maxit = 100)))
+}
+
+if ("all" %in% args) {
+    print("## full model (all)")
+    print(summary(nb7 <- glm.nb(len_1 ~ tds_1 + num_editors_1 + len_0 + num_editors_0 + tds_0 + lang, data = dat, maxit = 100)))
 }
