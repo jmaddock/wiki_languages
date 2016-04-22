ecc674b2e770124b0d69d5a59091bac088027ad1
Jim Maddock <skimaddster@gmail.com>
2016-04-22 13:35:31 -0500

    added script for sampling at specified quantile

--- scripts/qual_samples.py
+++ scripts/qual_samples.py
@@ -5,13 +5,13 @@ import pandas as pd
 ROOT_DIR = '../db/'
 EDIT_COUT_FILE_NAME = 'edit_counts.csv'
 
-def get_files(base_dir,target_name,debug=False):
+def get_files(base_dir,target_name,debug=v):
     files = []
     for root, directories, filenames in os.walk(base_dir):
         for filename in filenames:
             if target_name in filename and 'simple' not in root and 'combine' not in root:
                 files.append(os.path.join(root,filename))
-    if debug:
+    if v:
         print(files)
     return files
 
@@ -24,23 +24,25 @@ def get_quantiles(lang,quantiles):
     result = q.loc[quantiles]
     return result
 
-def get_pages(quantile,edit_counts,n):
+def get_pages(quantile,edit_counts,n,v):
     sample = edit_counts.loc[(edit_counts['namespace'] == 1) & (edit_counts['len'] > quantile)].sample(n=n)
-    print(sample)
+    if v:
+        print(sample)
     return sample
 
-def read_edit_counts(path):
+def read_edit_counts(path,v=False):
     edit_counts = pd.read_csv(path)
     edit_counts.len = edit_counts.len.astype(float)
-    print(edit_counts)
+    if v:
+        print(edit_counts)
     return edit_counts
     
 def main():
-    files = get_files('../db/','edit_counts.csv',debug=True)
+    files = get_files('../db/','edit_counts.csv',v=True)
     #get_quantiles('simple',[.5,.9,.99])
     for f in files:
         d = read_edit_counts(f)
-        get_pages(100,d,2)
+        get_pages(100,d,2,v=True)
     
 if __name__ == "__main__":
     main()
