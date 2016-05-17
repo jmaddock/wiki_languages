3a18cca8d119451a7bc431b760c4fba7ba47cdcb
Jim Maddock <skimaddster@gmail.com>
2016-05-16 20:56:13 -0500

    added cleaning script for english

---
+++ python_scripts/clean_en.py
@@ -0,0 +1,10 @@
+import config
+import os
+
+fname = os.path.join(config.ROOT_PROCESSED_DIR,'en',config.EDIT_COUNTS)
+df = pd.read_csv(fname,na_values={'title':''},keep_default_na=False,dtype={'title': object})
+df = df.set_index('page_id',drop=False)
+df = df.set_value(2474652, 'title', 'Sam Vincent (disambiguation)')
+df = df.set_value(1877838, 'title', 'Bugatti Chiron (disambiguation)')
+df = df.drop(df.index[[1826283,27902244]])
+df.to_csv(fname,na_rep='NaN',encoding='utf-8')
