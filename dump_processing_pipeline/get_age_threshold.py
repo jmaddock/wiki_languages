783cc3e3f1d26f2f57a3fbe5106c7e73c93ee990
Jim Maddock <skimaddster@gmail.com>
2016-12-15 10:42:34 -0800

    updated raw edits processing script with age threshold

--- dump_processing_pipeline/get_age_threshold.py
+++ dump_processing_pipeline/get_age_threshold.py
@@ -8,14 +8,19 @@ import argparse
 import config
 
 def get_last_date(infile_name):
+    # read raw edit csv
     df = utils.read_wiki_edits_file(infile_name)
+    # convert date string to datetime object
     df['ts'] = pd.to_datetime(df['ts'],format="%Y-%m-%d %H:%M:%S")
+    # get the last date in the csv
     last_date = df.ix[df['ts'].idxmax()]
     print(last_date)
     return last_date
 
 def get_lang_dirs_from_config():
+    # get list of languages from directory structure
     lang_list = [name for name in os.listdir(config.ROOT_PROCESSED_DIR) if (os.path.isdir(os.path.join(config.ROOT_PROCESSED_DIR,name)) and 'combined' not in name)]
+    # get full file path
     file_list = [os.path.join(config.ROOT_PROCESSED_DIR,lang,config.COMBINED_RAW_EDITS) for lang in lang_list]
     return file_list
 
@@ -26,11 +31,14 @@ def main():
     args = parser.parse_args()
     
     df = pd.DataFrame()
+    # get list of file paths
     infile_list = get_lang_dirs_from_config()
     for infile in infile_list:
         utils.log('processing {0}'.format(infile))
+        # get the datetime of the last edit
         df = df.append(get_last_date(infile))
-    df.sort_values(by='ts',ascending=True)
+    # sort the values
+    df = df.sort_values(by='ts',ascending=True)
     print(df)
     df.to_csv(args.outfile,index=False)
 
