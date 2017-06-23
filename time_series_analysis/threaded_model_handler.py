5b70ff9e5fa1127514d97f2dc2075498d4ad8a05
Jim Maddock <skimaddster@gmail.com>
2017-06-23 09:37:46 -0500

    changed threaded handler and stata script to import csv files

--- time_series_analysis/threaded_model_handler.py
+++ time_series_analysis/threaded_model_handler.py
@@ -21,7 +21,7 @@ class ThreadedModelHandler(object):
 
         def get_file_lists(self):
             if not self.debug_file:
-                self.infile_list = [os.path.join(self.indir,x) for x in os.listdir(self.indir) if x[-4:] == '.dta']
+                self.infile_list = [os.path.join(self.indir,x) for x in os.listdir(self.indir) if x[-4:] == '.csv']
                 pattern = re.compile('([0-9]+).csv')
                 self.outfile_list = [os.path.join(self.outdir,pattern.search(x).group(1)) for x in self.infile_list]
             else:
